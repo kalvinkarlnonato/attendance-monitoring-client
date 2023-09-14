7cec1d41370fc0f4a934261f7e731b0c1bcb534b
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2023-09-14 22:24:43 +0800

    Add project files.

---
+++ Biometric/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Biometric
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ToogleDarkModeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ToogleDarkModeButton
+            // 
+            this.ToogleDarkModeButton.Location = new System.Drawing.Point(-1, -1);
+            this.ToogleDarkModeButton.Name = "ToogleDarkModeButton";
+            this.ToogleDarkModeButton.Size = new System.Drawing.Size(75, 23);
+            this.ToogleDarkModeButton.TabIndex = 0;
+            this.ToogleDarkModeButton.Text = "Dark Mode";
+            this.ToogleDarkModeButton.UseVisualStyleBackColor = true;
+            this.ToogleDarkModeButton.Click += new System.EventHandler(this.ToogleDarkModeButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1006, 543);
+            this.Controls.Add(this.ToogleDarkModeButton);
+            this.Name = "MainForm";
+            this.Text = "Fingerprint Scanner Application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ToogleDarkModeButton;
+    }
+}
+
---
+++ Biometric/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Library;
+
+
+namespace Biometric
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadSizeResolution();
+        }
+        #region UI Style
+        private void LoadSizeResolution()
+        {
+            if (Properties.Settings.Default.Size.Width == 0) Properties.Settings.Default.Upgrade();
+            if (Properties.Settings.Default.Size.Width == 0 || Properties.Settings.Default.Size.Height == 0)
+            {
+                this.Size = new Size(1374, 749);
+                this.WindowState = FormWindowState.Normal;
+                this.Location = new Point(100, 70);
+            }
+            else
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.WindowState = FormWindowState.Normal;
+                }
+                this.WindowState = Properties.Settings.Default.State;
+                this.Location = Properties.Settings.Default.Location;
+                this.Size = Properties.Settings.Default.Size;
+                ToogleDarkMode(Properties.Settings.Default.IsDark);
+            }
+        }
+        private void SaveSizeRosolution()
+        {
+            Properties.Settings.Default.State = this.WindowState;
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Properties.Settings.Default.Location = this.Location;
+                Properties.Settings.Default.Size = this.Size;
+            }
+            else
+            {
+                Properties.Settings.Default.Location = this.RestoreBounds.Location;
+                Properties.Settings.Default.Size = this.RestoreBounds.Size;
+            }
+            Properties.Settings.Default.IsDark = Configuration.IsDark;
+            Properties.Settings.Default.Save();
+        }
+        private void ToogleDarkMode(Boolean isDark)
+        {
+            Configuration.IsDark = isDark;
+            Configuration.UseImmersiveDarkMode(Handle, isDark);
+            this.BackColor = Color.White;
+            if (isDark) this.BackColor = Color.FromArgb(31, 31, 31);
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Refresh();
+        }
+        private void ToogleDarkModeButton_Click(object sender, EventArgs e)
+        {
+            ToogleDarkMode(!Configuration.IsDark);
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSizeRosolution();
+        }
+        #endregion
+    }
+}
